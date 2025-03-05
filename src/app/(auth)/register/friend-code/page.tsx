e20d233b5c146916b7384929e93a3cddd5c0d133
Camden Spehl <47334268+CamdenSpehl@users.noreply.github.com>
2025-03-04 22:56:33 -0500

    padding fix

--- src/app/(auth)/register/friend-code/page.tsx
+++ src/app/(auth)/register/friend-code/page.tsx
@@ -171,7 +171,7 @@ export default function FriendCodePage() {
 
   return (
     <div className="container flex h-screen flex-col items-center justify-center">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px] px-4">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
             Add Your Friend Code
